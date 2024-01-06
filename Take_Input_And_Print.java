6e0803bc44cdcc9e40d1ad88754457239cb13e4e
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2024-01-06 09:48:34 +0530

    import java.util.Scanner; public class Input {     public static void main(String[] arg)     {         Scanner sc=new Scanner(System.in);         String a=sc.nextLine();         System.out.printf(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Input
+{
+    public static void main(String[] arg)
+    {
+        Scanner sc=new Scanner(System.in);
+        String a=sc.nextLine();
+        System.out.printf(a);
+    }
+}
