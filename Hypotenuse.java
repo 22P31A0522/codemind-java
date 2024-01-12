23b108c8de51f56d23cc23de9627e78891efcaf4
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2024-01-12 20:24:00 +0530

    import java.util.*; import java.lang.Math; public class Hypotenuse {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         System.out.printf("%.2f",Math.sqrt((a*a)+(b*b)));     } }

---
+++ Hypotenuse.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.lang.Math;
+public class Hypotenuse
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.printf("%.2f",Math.sqrt((a*a)+(b*b)));
+    }
+}
