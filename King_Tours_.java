0ac6425d242d72bfd56ba374b175a1461aaad554
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2024-01-31 17:44:58 +0530

    import java.util.*; public class King {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int m=sc.nextInt();         System.out.println(n*5+m*7);     } }

---
+++ King_Tours_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class King
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        System.out.println(n*5+m*7);
+    }
+}
