16d3605c5dbc149ab634957c02d5b5947cede353
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2024-01-31 17:40:16 +0530

    import java.util.*; public class Frames {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=sc.nextInt();         int a=(x+y)*2;         System.out.println(a*z);     } }

---
+++ Frames.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Frames
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=sc.nextInt();
+        int a=(x+y)*2;
+        System.out.println(a*z);
+    }
+}
