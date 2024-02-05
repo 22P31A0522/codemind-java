d53450de6917d3918e817f9216e55bbe108ada17
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2024-02-05 10:37:29 +0530

    import java.util.*; public class Inches {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         float x=sc.nextInt();         System.out.printf("%.2f",x*2.54);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Inches
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        float x=sc.nextInt();
+        System.out.printf("%.2f",x*2.54);
+    }
+}
