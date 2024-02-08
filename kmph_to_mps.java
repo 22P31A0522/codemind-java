ea42f5d6503164874505fb252808a47833a36153
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2024-02-08 18:17:12 +0530

    import java.util.*; public class Kmph {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         float k=sc.nextFloat();         double n=(5*k)/18;         System.out.printf("%.2f",n);     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Kmph
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        float k=sc.nextFloat();
+        double n=(5*k)/18;
+        System.out.printf("%.2f",n);
+    }
+}
