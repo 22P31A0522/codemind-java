a700dffb5e9a77386d74678214954f9de474dd39
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2024-02-28 10:37:21 +0530

    import java.util.*; public class Perfect {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int[] arr=new int[n];         int b = 0;         for(int i=0;i<n;i++)         {             arr[i]=sc.nextInt();         }         for(int i=0;i<n;i++)         {             int p=(int)Math.sqrt(arr[i]);             int a=p*p;             if(arr[i]==a)             {                 b +=arr[i];             }
            }
            System.out.println(b);
        }
    }

---
+++ Program_to_find_the_sum_of_perfect_square_elements_in_an_array.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Perfect
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        int b = 0;
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(int i=0;i<n;i++)
+        {
+            int p=(int)Math.sqrt(arr[i]);
+            int a=p*p;
+            if(arr[i]==a)
+            {
+                b +=arr[i];
+            }
+            
+        }
+        System.out.println(b);
+    }
+}
