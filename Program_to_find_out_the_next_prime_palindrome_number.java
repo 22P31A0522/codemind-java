d952de5beda00ebb4eec564e4f41d67e313aa5fd
22P31A0522 <120644027+22P31A0522@users.noreply.github.com>
2024-03-01 10:52:04 +0530

    import java.util.*; public class Palindrom {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int n=sc.nextInt();         int t,r=0,c,d;         while(true)         {             n++;             t=n;             while(t>0)             {                 r=r*10;                 r=r+t%10;                 t=t/10;             }             if(r==n)             {                 d = (int) Math.sqrt(n);                 for (c = 2; c <= d; c++) {                     if (n % c == 0)                         break;                 }                 if (c == d + 1)                     break;             }             r=0;         }         System.out.println(n);     } }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,29 +1,32 @@
-import java.io.*;
 import java.util.*;
-import java.lang.Math;
-public class NextPrimePalindrome {
-    public static void main(String[] args) {
-        int n, t, r = 0, c, d;
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        while (true) {
+public class Palindrom
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int t,r=0,c,d;
+        while(true)
+        {
             n++;
-            t = n;
-            while (t > 0) {
-                r = r * 10;
-                r = r + t % 10;
-                t = t / 10;
+            t=n;
+            while(t>0)
+            {
+                r=r*10;
+                r=r+t%10;
+                t=t/10;
             }
-            if (r == n) {
+            if(r==n)
+            {
                 d = (int) Math.sqrt(n);
                 for (c = 2; c <= d; c++) {
                     if (n % c == 0)
                         break;
                 }
                 if (c == d + 1)
-                    break; 
+                    break;
             }
-            r = 0; 
+            r=0;
         }
         System.out.println(n);
     }
